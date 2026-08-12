78d033237133403040cfa25650aba90ec024dfef
lucasGabrielDeAA <lg.technus@gmail.com>
2026-08-12 18:40:35 -0300

    tests directory added

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue'
+import CardModal from '@/components/CardModal.vue'
+</script>
+
+<template>
+  <div class="app-layout">
+    <Navbar />
+    <RouterView />
+    <CardModal />
+  </div>
+</template>
+
+<style>
+@import '@/assets/main.css';
+
+.app-layout {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
